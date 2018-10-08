2285eed0fbb06f146e66a29c3126646de913c6f3
JZiemnicki <37213900+JZiemnicki@users.noreply.github.com>
2018-10-08 07:21:03 -0400

    Add files via upload

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
+  handleCLick(){
+     this.props.clickHandler(this.props.button_text)
+  }
   render (){
     return (
-      <li><button>{this.props.button_text}</button></li>
+      <li><button onClick={this.handleCLick.bind(this)}>{this.props.button_text}</button></li>
     )
   }
 }
